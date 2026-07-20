56ff37a420f5037e3738019be8eacb17057a31c8
CodewithUtkarsh1020 <utkarshyadav00u@gmail.com>
2026-07-20 10:46:15 +0000

    [LeetCode Sync] Runtime - 1604 ms (5.00%), Memory - 56.3 MB (96.02%)

---
+++ solutions/0015-3sum/solution.java
@@ -0,0 +1,34 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+
+        Set<List<Integer>> ans = new HashSet<>();
+        int n = nums.length;
+
+
+        for (int i = 0; i < n; i++) {
+            Set<Integer> set = new HashSet<>();
+
+            for (int j = i + 1; j < n; j++) {
+                int third = 0 -(nums[i]+nums[j]);
+                if(set.contains(third)){
+
+                
+
+
+                        List<Integer> temp = new ArrayList<>();
+                        temp.add(nums[i]);
+                        temp.add(nums[j]);
+                        temp.add(third);
+
+                        Collections.sort(temp);
+
+                        ans.add(temp);
+                    
+                }
+                set.add(nums[j]);
+            }
+        }
+
+        return new ArrayList<>(ans);
+    }
+}
