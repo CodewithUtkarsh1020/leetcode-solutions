c7c7ebdf3f9abc03843d3f388e85ddd6693dec20
CodewithUtkarsh1020 <utkarshyadav00u@gmail.com>
2026-07-23 10:10:03 +0000

    [LeetCode Sync] Runtime - 18 ms (77.65%), Memory - 108 MB (63.60%)

---
+++ solutions/0217-contains-duplicate/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) 
+    {
+        int n = nums.length;
+     Set<Integer> st= new HashSet<>();
+     for(int i =0;i<n;i++)
+     {
+     if(st.contains(nums[i]))
+     {
+        return true;
+     }
+     
+
+     st.add(nums[i]);
+    
+     }
+    
+    return false;
+    }
+   
+}
