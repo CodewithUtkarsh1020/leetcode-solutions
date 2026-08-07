dcda315f764c448b447476f87d3f263cfc935134
CodewithUtkarsh1020 <utkarshyadav00u@gmail.com>
2026-08-07 12:10:42 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44.1 MB (59.78%)

---
+++ solutions/2876-number-of-employees-who-met-the-target/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numberOfEmployeesWhoMetTarget(int[] hours, int target)
+     
+     {
+        int targethours = 0;
+
+        int n = hours.length;
+
+        for(int i =0;i<n;i++)
+        {
+            if(hours[i]>=target)
+            {
+                targethours++;
+            }
+        }
+
+        return targethours;
+    }
+}
