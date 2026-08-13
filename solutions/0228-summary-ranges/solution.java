113a94ed852cd01f5e476ed4c601de49423c2595
CodewithUtkarsh1020 <utkarshyadav00u@gmail.com>
2026-08-13 10:21:48 +0000

    [LeetCode Sync] Runtime - 3 ms (78.19%), Memory - 43.2 MB (21.34%)

---
+++ solutions/0228-summary-ranges/solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<String> summaryRanges(int[] nums)
+     {
+        List<String> st = new ArrayList<>();
+
+        for(int i =0;i<nums.length;i++)
+        {
+            int start = nums[i];
+
+            while(i+1<nums.length && nums[i]+1==nums[i+1])
+            {
+                i++;
+            }
+            if(start == nums[i])
+            {
+                st.add(String.valueOf(start));
+
+            }
+            else
+            {
+                st.add(start +"->"+nums[i]);
+            }
+        }
+        
+        return st;
+    }
+}
