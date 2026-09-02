3ef04086a71f5a263d7cc929b0f6dde994cc9fa5
CodewithUtkarsh1020 <utkarshyadav00u@gmail.com>
2026-09-02 18:08:24 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43 MB (46.47%)

---
+++ solutions/0953-reverse-only-letters/solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String reverseOnlyLetters(String s) {
+        char[] ch = s.toCharArray();
+        int left = 0;
+        int right = ch.length - 1;
+
+        while (left < right) {
+            if (!Character.isLetter(ch[left])) {
+                left++;
+            } else if (!Character.isLetter(ch[right])) {
+                right--;
+            } else {
+                // Swap the characters when both are letters
+                char temp = ch[left];
+                ch[left] = ch[right];
+                ch[right] = temp;
+                left++;
+                right--;
+            }
+        }
+
+        return new String(ch);
+    }
+}
