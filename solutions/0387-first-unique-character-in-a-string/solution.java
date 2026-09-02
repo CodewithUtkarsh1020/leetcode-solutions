c32992d1a5f5834202e1f1df0da1c8f9d9426af6
CodewithUtkarsh1020 <utkarshyadav00u@gmail.com>
2026-09-02 04:26:56 +0000

    [LeetCode Sync] Runtime - 6 ms (84.94%), Memory - 46.6 MB (93.25%)

---
+++ solutions/0387-first-unique-character-in-a-string/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int firstUniqChar(String s) 
+    {
+        int [] frq = new int[26];
+       for(char str :s.toCharArray() )
+       {
+        frq[str-'a']++;
+       }
+       for(int i =0;i<s.length();i++)
+       {
+        if(frq[s.charAt(i)-'a']==1)
+        {
+            return i;
+        }
+       }
+       return -1;
+    }
+}
