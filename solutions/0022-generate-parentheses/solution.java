2bc57dc1ffb3733f2ea65d541a2b09170275f1d4
CodewithUtkarsh1020 <utkarshyadav00u@gmail.com>
2026-09-08 17:00:44 +0000

    [LeetCode Sync] Runtime - 2 ms (69.40%), Memory - 44.7 MB (54.97%)

--- solutions/0022-generate-parentheses/solution.java
+++ solutions/0022-generate-parentheses/solution.java
@@ -1,27 +1,26 @@
 class Solution {
-    //make a new function solve where we use recursion to solve the problem:
-    public void solve (int o,int c,String s, List<String>ans)
-    {
-        if(o==0 && c==o)
+     static void solve(int o,int c,String s,List<String> ans)
         {
-            ans.add(s);
-            return ;
+            if(o==0 && c==0)
+            {
+                ans.add(s);
+                return;
+            }
+            if(o>0)
+            {
+                solve(o-1,c,s+'(',ans);
+            }
+            if(c>o)
+            {
+                solve(o,c-1,s+')',ans);
+            }
         }
-      if(o>0){
-        solve (o-1, c, s+'(', ans);
-      }
-      if(c>o){
-        solve(o,c-1,s+')',ans);
-      }
-    }
-        
     public List<String> generateParenthesis(int n) 
     {
-        List<String> ans = new ArrayList<>();
-
+        List<String>ans = new ArrayList<>();
         solve(n,n,"",ans);
+       
         return ans;
     }
-
-        
+    
 }
