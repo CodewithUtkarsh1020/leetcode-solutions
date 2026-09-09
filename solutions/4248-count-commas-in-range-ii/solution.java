68ee9e3349a994f31416d4b7df25c85e36d352ce
CodewithUtkarsh1020 <utkarshyadav00u@gmail.com>
2026-09-09 05:19:05 +0000

    [LeetCode Sync] Runtime - 1 ms (99.36%), Memory - 42 MB (99.36%)

---
+++ solutions/4248-count-commas-in-range-ii/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public long countCommas(long n) 
+    {
+        long ans =0;
+        long start =1000;
+        long comma =1;
+
+        while(n>=start)
+        {
+            long end = Math.min(n,start*1000-1);
+            ans +=(end -start+1)*comma;
+            start*=1000;
+            comma++;
+        }
+        return ans;
+    }
+}
